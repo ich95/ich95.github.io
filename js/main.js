60a52ed5aa2114221b647b5a2b1a0cd8af0a89ac
ich95 <56219402+ich95@users.noreply.github.com>
2019-12-18 13:13:16 +0100

    build loader function
    Loader function displays current amounts based on localstorage values. Without function start values would be "0", even if vlaues are existent

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ function main(col) {
     localStorage.setItem(col, ++i);
     document.getElementById(col).innerHTML = i;
 }
+function loader() {
+    arr = ["weiss", "rot", "gelb", "blau", "dq"];
+    for (i = 0; i < arr.length; i++) {
+        document.getElementById(arr[i]).innerHTML = localStorage.getItem(arr[i]);
+        if (localStorage.getItem(arr[i]) === null) {
+            document.getElementById(arr[i]).innerHTML = 0;
 
+        }
+    }
+
+}
 function reset() {
     localStorage.clear();
     document.getElementById("weiss").innerHTML = 0;
@@ -14,17 +24,17 @@ function reset() {
     document.getElementById("dq").innerHTML = 0;
 }
 function quart() {
-    if (document.getElementById("quart").checked == false) {
+    if (document.getElementById("quart").checked == true) {
         var elem = document.getElementById("container");
         var g = document.getElementById("gDiv");
         var b = document.getElementById("bDiv");
         var r = document.getElementById("reset");
         var q = document.getElementById("dq");
-        r.style.gridRowStart="2";
-        r.style.gridRowEndt="2";
-        q.style.gridRowStart="2";
-        q.style.gridRowEnd="2";
-    
+        r.style.gridRowStart = "2";
+        r.style.gridRowEndt = "2";
+        q.style.gridRowStart = "2";
+        q.style.gridRowEnd = "2";
+
         elem.style.gridTemplateRows = "90% 10%";
         b.style.display = "none";
         g.style.display = "none";
@@ -35,10 +45,10 @@ function quart() {
         var b = document.getElementById("bDiv");
         var r = document.getElementById("reset");
         var q = document.getElementById("dq");
-        r.style.gridRowStart="3";
-        r.style.gridRowEndt="3";
-        q.style.gridRowStart="3";
-        q.style.gridRowEnd="3";
+        r.style.gridRowStart = "3";
+        r.style.gridRowEndt = "3";
+        q.style.gridRowStart = "3";
+        q.style.gridRowEnd = "3";
         elem.style.gridTemplateRows = "45% 45% 10%";
         g.style.display = "block";
         b.style.display = "block";
